ad1b23808c49118a6a31f4505df21eb1533d1910
Igor Khan <i_m@khanigo.pw>
2021-04-08 18:04:00 +1000

    no message

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+// import $ from 'jquery';
+// import * as PIXI from 'pixi.js'
+
+// require('./map');
+
+
+
+// let app = new PIXI.Application({width: window.innerWidth, height: window.innerHeight});
+// document.body.appendChild(app.view);
+
+// window.jQuery = jquery;
+// window.$ = jquery;
+
+// require('./header');
+// require('./nav');
+// require('./menu-panel');
+// require('./tab');
+// require('./slider');
+// require('./fancybox');
+
+// require('./ham');
+// require("@fancyapps/fancybox");
+
+
+// jquery(function($) {
+// });
